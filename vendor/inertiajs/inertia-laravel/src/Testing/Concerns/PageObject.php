10fca226e7635366562e53aebd71987d85248138
jhon corpuz <jvvcorpuz@addu.edu.ph>
2025-10-08 08:32:36 +0800

    update

--- vendor/inertiajs/inertia-laravel/src/Testing/Concerns/PageObject.php
+++ vendor/inertiajs/inertia-laravel/src/Testing/Concerns/PageObject.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Arr;
 use InvalidArgumentException;
 use PHPUnit\Framework\Assert as PHPUnit;
 
+/**
+ * @deprecated This trait is deprecated and will be removed in a future version.
+ * @see https://github.com/inertiajs/inertia-laravel/pull/338
+ */
 trait PageObject
 {
     public function component(?string $value = null, $shouldExist = null): self
@@ -49,6 +53,8 @@ trait PageObject
             'props' => $this->props,
             'url' => $this->url,
             'version' => $this->version,
+            'encryptHistory' => $this->encryptHistory,
+            'clearHistory' => $this->clearHistory,
         ];
     }
 }
